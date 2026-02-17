5cf405c15578cded56546d35d5c291925d6ca2c1
Naveenkumar Ammadapalli <ammadapallinaveen@gmail.com>
2026-02-17 13:39:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3006793800-37288",
+  "appId": "1:526397601868:web:28cbffe2e2848571761ce9",
+  "apiKey": "AIzaSyC5U36Td802xvA16CyAf-o4qBN932xP3xg",
+  "authDomain": "studio-3006793800-37288.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "526397601868"
+};
